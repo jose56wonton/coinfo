b66ef2cab6ef7187122ced5665abe62a4b8cb946
Joshua Wootonn <joshuawootonn@gmail.com>
2018-01-12 17:55:08 -0600

    deployed with hopefully a patch for loading in prod

--- src/containers/index/coin_table_container.js
+++ src/containers/index/coin_table_container.js
@@ -1,68 +1,58 @@
-
-import CoinTable from '../../components/index/coin_table';
-import React from 'react';
-import * as actions from '../../actions';
-import { connect } from 'react-redux';
-
+import CoinTable from "../../components/index/coin_table";
+import React from "react";
+import * as actions from "../../actions";
+import { connect } from "react-redux";
 
 class CoinTableContainer extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      order: 'asc',
-      orderBy: 'market_cap_usd',
-      selected: {},      
+      order: "asc",
+      orderBy: "market_cap_usd",
+      selected: {},
       page: 0,
-      rowsPerPage: 10,
+      rowsPerPage: 10
     };
-    if(this.props.coins.length === 0){   
-      this.props.fetchCoinList();
-    } 
   }
-
-  componentDidMount(){
-    if(this.props.coins.length === 0){   
-      this.props.fetchCoinList();
-    } 
+  componentWillMount() {
+    this.props.fetchCoinList();
   }
   handleRequestSort = (event, property) => {
     const orderBy = property;
-    let order = 'desc';
-    if (this.state.orderBy === property && this.state.order === 'desc') {
-      order = 'asc';
+    let order = "desc";
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
     }
-    this.props.sortCoinList(order,orderBy);  
+    this.props.sortCoinList(order, orderBy);
 
     this.setState({ order, orderBy });
   };
   handleChangePage = (event, page) => {
     this.setState({ page });
-    
   };
   handleChangeRowsPerPage = event => {
     this.setState({ rowsPerPage: event.target.value });
-    
   };
   render() {
-    return(
-    <CoinTable    
-    classes={this.props.classes}
-    data={this.props.coins}
-    order={this.state.order}
-    orderBy={this.state.orderBy}
-    selected={this.state.selected}
-    rowsPerPage={this.state.rowsPerPage}
-    page={this.state.page}   
-    handleRequestSort={this.handleRequestSort}
-    handleChangePage={this.handleChangePage}
-    handleChangeRowsPerPage={this.handleChangeRowsPerPage}
-    />
-    )
+    return (
+      <CoinTable
+        classes={this.props.classes}
+        data={this.props.coins}
+        order={this.state.order}
+        orderBy={this.state.orderBy}
+        selected={this.state.selected}
+        rowsPerPage={this.state.rowsPerPage}
+        page={this.state.page}
+        handleRequestSort={this.handleRequestSort}
+        handleChangePage={this.handleChangePage}
+        handleChangeRowsPerPage={this.handleChangeRowsPerPage}
+      />
+    );
   }
 }
 const mapStateToProps = (state, ownProps) => {
   return {
     coins: state.coins
-  }
-}
-export default connect(mapStateToProps,actions)(CoinTableContainer);
+  };
+};
+export default connect(mapStateToProps, actions)(CoinTableContainer);
